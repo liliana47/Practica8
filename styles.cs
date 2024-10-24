e9d882b7f65c7bf5b4a023a8a453405a34d38ad3
liliana47 <blnieves@unicesar.edu.co>
2024-10-23 20:32:10 -0500

    cambios

---
+++ styles.cs
@@ -0,0 +1,11 @@
+ h1{
+  color: rgb(123, 16, 16);
+  font-size: 24px;
+      }
+ p{
+    color: rgb(141, 19, 111);
+    font-family: Arial, Helvetica, sans-serif;
+    }
+img{
+     border: 4px solid rgb(210, 134, 4);
+}
